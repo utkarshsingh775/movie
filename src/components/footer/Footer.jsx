20f1f7a789ea86b5bdb4ab7780dad866a0880862
utkarsh <utkarsh757singh@gmail.com>
2023-07-08 23:51:33 +0530

    movie

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+               
+                <div className="infoText" style={{color: "white"}}>
+                Created with love by Utkarsh Singh
+                   
+                </div>
+                <div className="socialIcons">
+                <a href="https://instagram.com/utkarsh._.singh._?igshid=ZDc4ODBmNjlmNQ==" target="_blank">
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    </a>
+                  
+                    <a href="https://www.linkedin.com/in/utkarsh-singh-841810248/" target="_blank">  <span className="icon" >
+                        <FaLinkedin />
+                    </span>
+                    </a>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
